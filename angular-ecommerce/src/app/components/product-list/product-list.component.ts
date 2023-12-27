142acce7d11b3b2be89721af276208fbb352bd60
ZekaCedar <zulaikha.affandi@gmail.com>
2023-12-27 23:44:56 +0800

    complete Integrating online shop template

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -4,26 +4,23 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list-grid.component.html',
+  //templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.listProducts();
   }
 
-  listProducts(){
-    this.productService.getProductList().subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
   }
-
 }
-
--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -4,24 +4,23 @@ import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private baseUrl = 'http://localhost:8080/api/products';
+  //private baseUrl = 'http://localhost:8080/api/products?size=100';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  getProductList():Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-      map(response => response._embedded.products)
-    );
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
   }
-
 }
 
-interface GetResponse{
+interface GetResponse {
   _embedded: {
     products: Product[];
-  }
+  };
 }
